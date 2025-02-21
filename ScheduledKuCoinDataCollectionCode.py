0c4b6aa90fcaa6a61e7257a8874da7394e8ed956
Fedric-Reform <fedric@reformdao.com>
2025-02-21 21:33:40 +0700

    Update ScheduledKuCoinDataCollectionCode.py

--- ScheduledKuCoinDataCollectionCode.py
+++ ScheduledKuCoinDataCollectionCode.py
@@ -122,7 +122,7 @@ def main():
     df_output = pd.DataFrame(results)
 
     # Save the data to a new Excel file with a timestamp in the filename
-    output_file = f"KuCoinData{datetime.now().strftime('%Y%m%d')}.xlsx"
+    output_file = f"KuCoinData.xlsx"
     df_output.to_excel(output_file, index=False)
 
     print(f"✅ Data saved to {output_file}")
