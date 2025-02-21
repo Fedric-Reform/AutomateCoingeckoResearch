3ace81170b554b5d35f6b334d08b58aaafd1fed0
Fedric-Reform <fedric@reformdao.com>
2025-02-21 21:35:22 +0700

    Update ScheduledOkxDataCollectionCode.py

--- ScheduledOkxDataCollectionCode.py
+++ ScheduledOkxDataCollectionCode.py
@@ -121,7 +121,7 @@ def main():
     df_output = pd.DataFrame(results)
 
     # Save the data to a new Excel file with a timestamp in the filename
-    output_file = f"OkxData{datetime.now().strftime('%Y%m%d')}.xlsx"
+    output_file = f"OkxData.xlsx"
     df_output.to_excel(output_file, index=False)
 
     print(f"✅ Data saved to {output_file}")
